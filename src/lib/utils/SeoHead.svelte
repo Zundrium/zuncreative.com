e5a4658cefd51b2b268cd9bf4400e67024f866a7
Sem Verbraak <sem@zuncreative.com>
2025-06-06 19:12:15 +0200

    -a

--- src/lib/utils/SeoHead.svelte
+++ src/lib/utils/SeoHead.svelte
@@ -1,37 +1,39 @@
 <script module lang="ts">
-	const social_images: any = import.meta.glob(
-		["../assets/images/**/*.{avif,gif,heif,jpeg,jpg,png,tiff,webp}"],
-		{
-			eager: true,
-			query: {
-				enhanced: true,
-				format: "jpeg",
-				w: "1200",
-				h: "630",
-			},
+const social_images: any = import.meta.glob(
+	["../assets/images/**/*.{avif,gif,heif,jpeg,jpg,png,tiff,webp}"],
+	{
+		eager: true,
+		query: {
+			enhanced: true,
+			format: "jpeg",
+			w: "1200",
+			h: "630",
 		},
-	);
+	},
+);
 
-	const get_social_image = (desired_image: string) => {
-		return social_images[`../assets${desired_image}`].default.img.src;
-	};
+const domain = import.meta.env.VITE_DOMAIN;
+
+const get_social_image = (desired_image: string) => {
+	return `https://${domain}${social_images[`../assets${desired_image}`].default.img.src}`;
+};
 </script>
 
 <script lang="ts">
-	import { formatDateToISO8601 } from "$lib/utils/formatDate";
-	import type { SeoHeadProps } from "$lib/utils/types";
+import { formatDateToISO8601 } from "$lib/utils/formatDate";
+import type { SeoHeadProps } from "$lib/utils/types";
 
-	const domain = import.meta.env.VITE_DOMAIN;
+const domain = import.meta.env.VITE_DOMAIN;
 
-	export let props: SeoHeadProps;
+export let props: SeoHeadProps;
 
-	if (!props.datePublished) {
-		props.datePublished = new Date().toISOString();
-	}
+if (!props.datePublished) {
+	props.datePublished = new Date().toISOString();
+}
 
-	if (!props.dateModified) {
-		props.dateModified = new Date().toISOString();
-	}
+if (!props.dateModified) {
+	props.dateModified = new Date().toISOString();
+}
 </script>
 
 <svelte:head>
