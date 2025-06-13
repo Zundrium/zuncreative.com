8149a38ef929a61ea6af890eae4069a72bdb0b3a
Sem Verbraak <sem@zuncreative.com>
2025-06-13 11:36:38 +0200

    fix: Refine copy in services section

--- src/lib/components/sections/index/Services.svelte
+++ src/lib/components/sections/index/Services.svelte
@@ -17,7 +17,7 @@ import { viewportSlideInBottom } from "$lib/utils/viewportSwitchClass";
 	/>
 
 <AnimatedParagraph class="text-center w-full md:w-1/2">
-			Ik ben <i>Sem</i>, een web developer met meer dan 15 jaar ervaring uit Amsterdam. Ik maak audiovisuele websites en web applicaties die snel, toegankelijk en gebruiksvriendelijk zijn. Mijn focus ligt op het creëren van een geweldige gebruikerservaring door middel van moderne technologieën en designprincipes.
+			Ik ben <i>Sem</i>, een web developer met meer dan 15 jaar ervaring uit Amsterdam. Ik maak audiovisuele websites en web applicaties die snel, toegankelijk en gebruiksvriendelijk zijn. Mijn focus ligt op het creëren van een onvergetelijke gebruikerservaring door middel van moderne technologieën en designprincipes.
 		</AnimatedParagraph>
 
 	<div 
@@ -26,19 +26,19 @@ import { viewportSlideInBottom } from "$lib/utils/viewportSwitchClass";
 		<div class="text-center flex flex-col gap-4 lg:gap-6 xl:gap-12">
 			<h3 class="text-6xl md:text-7xl lg:text-8xl xl:text-9xl serif-display" use:viewportSlideInBottom>AI</h3>
 			<AnimatedParagraph>
-				Alles is te versnellen, verbeteren en te personaliseren met Artificial Intelligence. Ik pas het toe in al mijn recente werk. Maar het kan geen verantwoordelijkheid nemen.
+				Alles is te versnellen, verbeteren en te personaliseren met Artificial Intelligence. Ik pas het toe in al mijn recente werk. 
 			</AnimatedParagraph>
 		</div>
 		<div class="text-center flex flex-col gap-4 lg:gap-6 xl:gap-12">
 			<h3 class="text-6xl md:text-7xl lg:text-8xl xl:text-9xl serif-display" use:viewportSlideInBottom>AR</h3>
 			<AnimatedParagraph>
-				Al je ervaringen met Augmented Reality vergeet je niet, in combinatie met de laatste AI trends kan je iedereen een persoonlijke ervaring geven.
+				Ervaringen met Augmented Reality vergeet je niet, in combinatie met de laatste AI trends kan je een persoonlijke ervaring bieden.
 			</AnimatedParagraph>
 		</div>
 		<div class="text-center flex flex-col gap-4 lg:gap-6 xl:gap-12">
 			<h3 class="text-6xl md:text-7xl lg:text-8xl xl:text-9xl serif-display" use:viewportSlideInBottom>3D</h3>
 			<AnimatedParagraph>
-				Een meer toegankelijke, visueel rijke ervaring kan gewoon in je browser, zoals de header hierboven. Elk concept, elke cinematische uitdrukking is te zien op een web pagina.
+				Een audiovisuele ervaring kan direct in je browser met een enkele klik. Een cinematisch verhaal dat toegankelijk is voor iedereen.
 			</AnimatedParagraph>
 		</div>
 
